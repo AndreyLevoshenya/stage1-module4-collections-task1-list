4b86aaed2f2aa3a717b7d030a436f06128cf5d1e
Andrey <98894778+AndreyLevoshenya@users.noreply.github.com>
2023-04-12 14:33:12 +0300

    Add files via upload

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> oddThanEvenList = new LinkedList<>();
+        for (Integer integer : sourceList) {
+            if (integer % 2 == 0) {
+                oddThanEvenList.addLast(integer);
+            } else {
+                oddThanEvenList.addFirst(integer);
+            }
+        }
+        return oddThanEvenList;
     }
 }
